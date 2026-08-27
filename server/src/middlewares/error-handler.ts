740fd3f577f85006d6db2e72907b1760bde57892
Prithivi <prithivi@users.noreply.github.com>
2026-08-27 09:48:42 +0545

    feat: add async handler middleware and improve error handling with HTTP status codes

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,13 +1,16 @@
 import type { ErrorRequestHandler } from "express";
+import { HTTP_STATUS } from "../config/http.config";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(`Error occured on path ${req.path}:`, err);
 
   if (err instanceof SyntaxError) {
-    return res.status(400).json({ message: "Invalid JSON payload" });
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Invalid JSON payload" });
   }
 
-  return res.status(500).json({
+  return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
     message: "An unexpected error occurred. Please try again later.",
     error: err?.message || "Internal Server Error",
   });
